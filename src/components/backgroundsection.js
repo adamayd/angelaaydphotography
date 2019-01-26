6a19ea1ee6345828207ab019c5b606f30211bf40
Adam Ayd <adam.ayd@gmail.com>
2019-01-25 23:06:02 -0500

    framed out header section on main page

---
+++ src/components/backgroundsection.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
+import styled from 'styled-components';
+import BackgroundImage from 'gatsby-background-image'
+
+const BackgroundSection = ({ className }) => (
+  <StaticQuery query={graphql`
+    query {
+      desktop: file(relativePath: { eq: "portrait-girl.jpg"}) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 4160) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `}
+  render={data => {
+    // Set ImageData
+    const imageData = data.desktop.childImageSharp.fluid
+    return (
+      <BackgroundImage Tag="section"
+                       className={className}
+                       fluid={imageData}
+                       backgroundColor={`#040E18`}
+      >
+        <h1>Angela Ayd Photography BGImg</h1>
+      </BackgroundImage>
+    )
+  }
+  }
+  />
+)
+
+const StyledBackgroundSection = styled(BackgroundSection)`
+  width: 100%;
+  background-repeat: repeat-y;
+`
+
+export default StyledBackgroundSection
--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React from 'react';
 // import NavBar from './navbar';
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+// import StyledBackgroundSection from './backgroundsection';
+
+import bgImg from '../images/portrait-girl.jpg';
 
 const Header = ({ siteTitle, menuLinks }) => (
   <div
     style={{
-      background: `teal`,
-      marginBottom: `1.45rem`
+      backgroundImage: `linear-gradient(rgba(0,0,0,0.3),rgba(255,255,255,0.2)), url(${bgImg})`,
+      marginBottom: `1.45rem`,
+      height: '100vh',
+      backgroundAttachment: 'fixed',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover'
     }}
     >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        position: 'fixed',
+        width: `100%`,
         padding: `0.8rem 1.0875rem`,
+        borderBottom: `1px rgba(255,255,255,0.75) solid`
       }}
     >
-      <nav style={{ display: 'flex', justifyContent: 'center', }}>
+      <nav style={{ display: 'flex', justifyContent: 'center' }}>
         {
           menuLinks.map(link => 
             <li key={link.name} style={{ 'listStyleType': 'none', marginBottom: 0 }}>
