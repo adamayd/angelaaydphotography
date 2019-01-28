68fcf8b33f47e436ee042a42e6e5bd8ebf4432e7
Adam Ayd <adam.ayd@gmail.com>
2019-01-27 20:42:46 -0500

    reconfigured header and nav for all pages with hero on home

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,14 @@ import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 // import StyledBackgroundSection from './backgroundsection';
-
 import bgImg from '../images/portrait-girl.jpg';
 
-const Header = ({ siteTitle, menuLinks }) => (
+const Header = ({ siteTitle, menuLinks, location }) => (
   <div
     style={{
       backgroundImage: `linear-gradient(rgba(0,0,0,0.3),rgba(255,255,255,0.2)), url(${bgImg})`,
       marginBottom: `1.45rem`,
-      height: '100vh',
+      height: location.pathname === "/" ? '100vh' : '25vh',
       backgroundAttachment: 'fixed',
       backgroundPosition: 'center',
       backgroundSize: 'cover'
@@ -23,7 +22,8 @@ const Header = ({ siteTitle, menuLinks }) => (
         position: 'fixed',
         width: `100%`,
         padding: `0.8rem 1.0875rem`,
-        borderBottom: `1px rgba(255,255,255,0.75) solid`
+        borderBottom: `1px rgba(0,0,0,0.75) solid`,
+        backgroundColor: 'white'
       }}
     >
       <nav style={{ display: 'flex', justifyContent: 'center' }}>
@@ -32,7 +32,7 @@ const Header = ({ siteTitle, menuLinks }) => (
             <li key={link.name} style={{ 'listStyleType': 'none', marginBottom: 0 }}>
               <Link to={link.link}
                 style={{
-                  color: `white`,
+                  color: `black`,
                   textDecoration: `none`,
                   textAlign: `left`,
                   padding: '20px',
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,45 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
-
+import { Location } from '@reach/router';
 import Header from './header';
 import Footer from './footer';
 import '../css/layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title,
-            description,
-            menuLinks {
-              name,
-              link
+  <Location>
+    {({ location }) => (
+      <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title,
+              description,
+              menuLinks {
+                name,
+                link
+              }
             }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Header menuLinks={data.site.siteMetadata.menuLinks} siteTitle={data.site.siteMetadata.title} />
-        {console.log(data)}
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-        <Footer />
-      </>
+      `}
+      render={data => (
+        <>
+          <Header 
+            menuLinks={data.site.siteMetadata.menuLinks} 
+            siteTitle={data.site.siteMetadata.title} 
+            location={location} 
+          />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            {children}
+          </div>
+          <Footer />
+        </>
+      )}
+    /> 
     )}
-  />
+  </Location>
 )
 
 Layout.propTypes = {
