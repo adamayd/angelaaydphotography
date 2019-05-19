633795588af2b3135fa5f601f12b90bfce45510d
Adam Ayd <adamayd@gmail.com>
2019-05-19 13:36:01 -0400

    added eslint to project

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
@@ -21,6 +22,10 @@ export default function Template({
       </div>
     </Layout>
   )
+}
+
+Template.propTypes = {
+  data: PropTypes.node.isRequired,
 }
 
 export const pageQuery = graphql`
