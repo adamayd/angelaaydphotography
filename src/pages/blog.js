0bae27fba93b3691a530c4d1a355f4029a7eb0ce
Adam Ayd <adamayd@gmail.com>
2019-05-19 19:03:48 -0400

    added basic blog listing for GraphQL query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-export default () => (
+const Blog = ({ data }) => (
   <Layout>
     <SEO title="Blog" keywords={[`Angela Ayd Photography`, `blog`]} />
-    <div style={{ color: `teal` }}>
+    <div>
       <h1>Angela Ayd Photography Blog</h1>
       <p>Blogging about things in the photography-sphere</p>
+      <p>{data.allMarkdownRemark.totalCount}</p>
+      <ul>
+        {data.allMarkdownRemark.edges.map((edge) => {
+          return (
+            <li key={edge.node.id}>
+              <h2>{edge.node.frontmatter.title} <span style={{ fontSize: `0.7em` }}>{edge.node.frontmatter.date}</span></h2>            
+              <p>{edge.node.excerpt}</p>
+            </li>
+        )})}
+      </ul>
     </div>
   </Layout>
 )
+
+export default Blog
+
+Blog.propTypes = {
+  data: PropTypes.node.isRequired
+}
+
+export const postsQuery = graphql`
+  query {
+  allMarkdownRemark {
+    totalCount
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          path
+          date
+        }
+        excerpt
+      }
+    }
+  }
+}
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h2>Latest Works</h2>
-    <p>Lorem ipsum dolor amet celiac synth microdosing health goth raclette +1 master cleanse kogi. PBR&amp;B typewriter mixtape gastropub tbh cronut bushwick lo-fi whatever thundercats messenger bag taxidermy cardigan. Sriracha health goth fashion axe brunch occupy dreamcatcher try-hard tacos taxidermy. Narwhal paleo fam butcher mlkshk swag. Photo booth enamel pin leggings, copper mug PBR&amp;B gochujang portland bitters mlkshk you probably haven't heard of them fingerstache flexitarian.</p>
+    <p>Lorem ipsum dolor amet celiac synth microdosing health goth raclette +1 master cleanse kogi. PBR&amp;B typewriter mixtape gastropub tbh cronut bushwick lo-fi whatever thundercats messenger bag taxidermy cardigan. Sriracha health goth fashion axe brunch occupy dreamcatcher try-hard tacos taxidermy. Narwhal paleo fam butcher mlkshk swag. Photo booth enamel pin leggings, copper mug PBR&amp;B gochujang portland bitters mlkshk you probably haven&apos;t heard of them fingerstache flexitarian.</p>
   </Layout>
 )
 
